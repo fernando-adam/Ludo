bc4baf4adfc642e86aceea2acd7327144ebb3728
fernando-adam <adam.fernando@hotmail.com>
2023-01-02 15:11:37 -0300

    Migration of new columns of Game and User. #3

--- Ludo.Infra/Persistence/Migrations/20230102180947_SecondMigration.Designer.cs
+++ Ludo.Infra/Persistence/Migrations/20230102180947_SecondMigration.Designer.cs
@@ -4,6 +4,7 @@ using Ludo.Infra.Persistance;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Ludo.Infra.Persistence.Migrations
 {
     [DbContext(typeof(LudoDbContext))]
-    partial class LudoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230102180947_SecondMigration")]
+    partial class SecondMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Ludo.Infra/Persistence/Migrations/20230102180947_SecondMigration.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ludo.Infra.Persistence.Migrations
+{
+    public partial class SecondMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumberOfPlayers",
+                table: "Games");
+
+            migrationBuilder.RenameColumn(
+                name: "Age",
+                table: "Games",
+                newName: "MinimumNumberOfPlayers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaximumNumberOfPlayers",
+                table: "Games",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MinimumAge",
+                table: "Games",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "MaximumNumberOfPlayers",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "MinimumAge",
+                table: "Games");
+
+            migrationBuilder.RenameColumn(
+                name: "MinimumNumberOfPlayers",
+                table: "Games",
+                newName: "Age");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NumberOfPlayers",
+                table: "Games",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
